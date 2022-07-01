be9165f191b43e9426fe8e430b1744302c412dd0
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-07-01 21:38:07 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@
 
 const express = require('express')
 
+const connect = require("./config/db")
+
+const StudentController = require("./Controller/user.controller")
+
+
+ 
 const app = express()
-
-
 app.use(express.json())
 
 
-app.listen(3001,async()=>{
+
+
+app.use("",StudentController)
+
+
+
+
+const port = process.env.PORT || 3001
+
+app.listen(port,async()=>{
     try{
-        console.log("listening on 3001")
+        await connect()
+        console.log("listening on 3002")
+    }
+    catch(err){
+        console.log(err.message)
     }
 })
